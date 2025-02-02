720a89a7ef47bf2913de01644841e2b0ff52467c
Sydney <sydneyrpenix@students.abtech.edu>
2025-02-02 15:51:59 -0500

    Create FK constraints in database

--- culinnari.sql
+++ culinnari.sql
@@ -68,8 +68,8 @@ CREATE TABLE `ingredient` (
   `ingredient_quantity` DECIMAL(5,2) CHECK(ingredient_quantity > 0),
   `measurement_id` INT,
   `recipe_id` INT,
-  FOREIGN KEY (measurement_id) REFERENCES measurement(measurement_id) ON DELETE CASCADE,
-  FOREIGN KEY (recipe_id) REFERENCES recipe(recipe_id) ON DELETE CASCADE
+  CONSTRAINT ingredient_fk_measurement FOREIGN KEY (measurement_id) REFERENCES measurement(measurement_id) ON DELETE CASCADE,
+  CONSTRAINT ingredient_fk_recipe FOREIGN KEY (recipe_id) REFERENCES recipe(recipe_id) ON DELETE CASCADE
 );
 
 CREATE TABLE `measurement` (
@@ -82,19 +82,13 @@ CREATE TABLE `step` (
   `recipe_id` INT,
   `step_number` INT,
   `step_description` text,
-  FOREIGN KEY (recipe_id) REFERENCES 
+  CONSTRAINT step_fk_recipe FOREIGN KEY (recipe_id) REFERENCES recipe(recipe_id) ON DELETE CASCADE 
 );
 
 CREATE TABLE `diet` (
   `diet_id`  INT PRIMARY KEY AUTO_INCREMENT,
   `diet_name` VARCHAR(50),
   `diet_icon_url` VARCHAR(255)
-);
-
-CREATE TABLE `recipe_meal_type` (
-  `recipe_meal_type_id`  INT PRIMARY KEY AUTO_INCREMENT,
-  `recipe_id` INT,
-  `meal_type_id` INT
 );
 
 CREATE TABLE `meal_type` (
@@ -107,27 +101,41 @@ CREATE TABLE `style` (
   `style_name` VARCHAR(50)
 );
 
+CREATE TABLE `recipe_meal_type` (
+  `recipe_meal_type_id`  INT PRIMARY KEY AUTO_INCREMENT,
+  `recipe_id` INT,
+  `meal_type_id` INT,
+  CONSTRAINT recipe_meal_type_fk_recipe FOREIGN KEY (recipe_id) REFERENCES recipe(recipe_id) ON DELETE CASCADE,
+  CONSTRAINT recipe_meal_type_fk_meal_type FOREIGN KEY (meal_type_id) REFERENCES meal_type(meal_type_id) ON DELETE CASCADE
+);
+
 CREATE TABLE `cookbook` (
   `cookbook_id` INT  INT PRIMARY KEY AUTO_INCREMENT,
   `cookbook_name` VARCHAR(50),
-  `user_id` INT
+  `user_id` INT,
+  CONSTRAINT cookbook_fk_user FOREIGN KEY (user_id) REFERENCES user(user_id) ON DELETE CASCADE
 );
 
 CREATE TABLE `cookbook_recipe` (
   `cookbook_recipe_id`  INT PRIMARY KEY AUTO_INCREMENT,
   `cookbook_id` INT,
-  `recipe_id` INT
+  `recipe_id` INT,
+  CONSTRAINT cookbook_recipe_fk_cookbook FOREIGN KEY (cookbook_id) REFERENCES cookbook(cookbook_id) ON DELETE CASCADE,
+  CONSTRAINT cookbook_recipe_fk_recipe FOREIGN KEY (recipe_id) REFERENCES recipe(recipe_id) ON DELETE CASCADE
 );
 
 CREATE TABLE `recipe_diet` (
   `recipe_diet_id`  INT PRIMARY KEY AUTO_INCREMENT,
   `recipe_id` INT,
-  `diet_id` INT
+  `diet_id` INT,
+  CONSTRAINT recipe_diet_fk_recipe FOREIGN KEY (recipe_id) REFERENCES recipe(recipe_id) ON DELETE CASCADE,
+  CONSTRAINT recipe_diet_fk_diet FOREIGN KEY (diet_id) REFERENCES diet(diet_id) ON DELETE CASCADE
 );
 
 CREATE TABLE `recipe_style` (
   `recipe_style_id`  INT PRIMARY KEY AUTO_INCREMENT,
   `recipe_id` INT,
-  `style_id` INT
+  `style_id` INT,
+  CONSTRAINT recipe_style_fk_recipe FOREIGN KEY (recipe_id) REFERENCES recipe(recipe_id) ON DELETE CASCADE,
+  CONSTRAINT recipe_style_fk_style FOREIGN KEY (style_id) REFERENCES style(style_id) ON DELETE CASCADE
 );
-
