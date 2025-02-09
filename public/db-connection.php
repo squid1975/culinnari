429658d067f0588a95c53d7fac8b2a1ca26c2c0f
Sydney <sydneyrpenix@students.abtech.edu>
2025-02-09 15:56:36 -0500

    Change title of page

--- public/db-connection.php
+++ public/db-connection.php
@@ -5,7 +5,7 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Culinnari Home</title>
+        <title>Culinnari: Database Connection</title>
         <link href="css/styles.css" rel="stylesheet">
         <script src="js/script.js" defer></script>
     </head>
